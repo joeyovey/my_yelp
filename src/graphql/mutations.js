0301beb70fffb9798434fa9c6b9679e242e4a6b6
hr1 <hr1@outsource.ng>
2024-09-26 07:52:20 +0100

    Defined createResturant and deleteResturant functions

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -49,3 +49,34 @@ export const deleteTodo = /* GraphQL */ `
     }
   }
 `;
+
+export const createRestaurant = /* GraphQL */ `
+  mutation CreateRestaurant(
+    $input: CreateRestaurantInput!
+    $condition: ModelRestaurantConditionInput
+  ) {
+    createRestaurant(input: $input, condition: $condition) {
+      id
+      name
+      description
+      location
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+
+export const deleteRestaurant = /* GraphQL */ `
+  mutation DeleteRestaurant($input: DeleteRestaurantInput!, $condition: ModelRestaurantConditionInput) {
+    deleteRestaurant(input: $input, condition: $condition) {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
